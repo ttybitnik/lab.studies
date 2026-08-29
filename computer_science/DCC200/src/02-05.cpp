3010d23c20a4766bf22643036162c69fd9b9d10f
Vinícius Moraes <vinicius.moraes@eternodevir.com>
2026-08-29 11:00:34 -0300

    style(dcc): add missing space before brace

--- computer_science/DCC200/src/02-05.cpp
+++ computer_science/DCC200/src/02-05.cpp
@@ -18,7 +18,7 @@ int main()
 			cin >> mat[i][j];
 
 	cout << "Matrix: " << endl;
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++)
             cout << mat[i][j] << "\t";
 		cout << endl;
